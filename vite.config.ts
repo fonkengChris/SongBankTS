7582b9e3a3ee775afc28419c99543e931f654627
fonkengChris <echristianfonkeng@gmail.com>
2025-01-16 23:11:20 +0000

    adjust configurations for env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,27 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    "import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID": JSON.stringify(
-      process.env.VITE_GOOGLE_OAUTH_CLIENT_ID
-    ),
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: "https://sheet-music-library-ad225c202768.herokuapp.com",
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    define: {
+      "import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID": JSON.stringify(
+        env.VITE_GOOGLE_OAUTH_CLIENT_ID
+      ),
+    },
+    server: {
+      port: 5173,
+      proxy: {
+        "/api": {
+          target: "https://sheet-music-library-ad225c202768.herokuapp.com",
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  };
 });
