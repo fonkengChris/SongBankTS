6b7888138a405a8fd097db134e7cdcdf7887c109
fonkengChris <echristianfonkeng@gmail.com>
2025-07-16 00:07:11 +0100

    updated filtering logic 2

--- src/data/languages.ts
+++ src/data/languages.ts
@@ -1,36 +1,36 @@
 export default [
   {
-    _id: "675e06ad969aa4c609424652",
+    _id: "686ed22b0f6bf3ecdae5eeea",
     name: "English",
     code: "EN",
     __v: 0,
   },
   {
-    _id: "675e06dd969aa4c609424654",
+    _id: "686ed25f0f6bf3ecdae5eeeb",
     name: "French",
     code: "FR",
     __v: 0,
   },
   {
-    _id: "675e0737969aa4c60942465c",
-    name: "Italiano",
+    _id: "686ed2d4e84296be161e5205",
+    name: "Italian",
     code: "IT",
     __v: 0,
   },
   {
-    _id: "675e070d969aa4c609424658",
+    _id: "686ed2a40f6bf3ecdae5eeec",
     name: "Latin",
     code: "LT",
     __v: 0,
   },
   {
-    _id: "675e0724969aa4c60942465a",
+    _id: "686ed2bbe84296be161e5204",
     name: "Portoguese",
     code: "PR",
     __v: 0,
   },
   {
-    _id: "675e06f7969aa4c609424656",
+    _id: "686ed2801f0fc80c5e1530c2",
     name: "Spanish",
     code: "ES",
     __v: 0,
